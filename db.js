05a75e6194e9bdd0a39e0581b6b368306716c1e0
David Ames ubnt desktop <readytowin2298@gmail.com>
2021-04-20 01:23:06 -0500

    finished

--- db.js
+++ db.js
@@ -2,9 +2,14 @@
 
 
 const { Client } = require("pg");
-const { DB_URI } = require("./config");
+const { DB_URI, DB_PASSWORD } = require("./config");
 
-const client = new Client(DB_URI);
+const client = new Client({
+    host : DB_URI,
+    port : 5334,
+    user : 'postgres',
+    password : DB_PASSWORD
+});
 
 client.connect();
 
